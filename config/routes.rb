4b619719096784dfe4f2b56ef2e4aa8e7c67d1cf
bdanilin <danilin003@gmail.com>
2017-06-15 09:17:21 -0700

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,11 @@ Rails.application.routes.draw do
  
  get "/" => "cars#index"
  get "/cars" => "cars#index"
- 
  get "/cars/new" => "cars#new"
- 
  post "/cars" => "cars#create"
-
  get "/cars/:id" => "cars#show"
-
  get "/cars/:id/edit" => "cars#edit"
-
  patch "/cars/:id" => "cars#update"
-
  delete "/cars/:id" => "cars#destroy"
 
  get "/deals" => "cars#deals"
